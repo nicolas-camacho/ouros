229dbf9bcba4055ad43acd4228f9e86bb4b095f7
Nicolas Camacho Aguilar <nicolas.aguilar1999@gmail.com>
2025-06-22 22:48:57 -0500

    feat: base score mechanic added

--- conf.lua
+++ conf.lua
@@ -2,4 +2,5 @@ function love.conf(t)
     t.window.title = "Ouros"
     t.window.width = 1280
     t.window.height = 720
+    t.console = true
 end
--- main.lua
+++ main.lua
@@ -14,9 +14,26 @@ function love.load()
         height = 25,
         speed = 200
     }
+
+    POINT = {
+        x = 0,
+        y = 0,
+        width = 25,
+        height = 25,
+        exists = false,
+        timer = 0,
+        collided = false
+    }
+
+    SCORE = 0
 end
 
 function love.update(dt)
+
+    if POINT.exists == false then
+        POINT.timer = POINT.timer + 1
+    end
+
     if love.keyboard.isDown("up") and PLAYER.y > INITIAL_Y then
         PLAYER.y = PLAYER.y - PLAYER.speed * dt
     end
@@ -32,11 +49,25 @@ function love.update(dt)
     if love.keyboard.isDown("right") and PLAYER.x < INITIAL_X + (500 - PLAYER.width) then
         PLAYER.x = PLAYER.x + PLAYER.speed * dt
     end
+
+    if POINT.exists == false and POINT.timer > 60 then
+        POINT.x = math.random(INITIAL_X, INITIAL_X + 500 - POINT.width)
+        POINT.y = math.random(INITIAL_Y, INITIAL_Y + 500 - POINT.height)
+        POINT.exists = true
+        POINT.timer = 0
+        POINT.collided = false
+    end
+
+    CollideWithPoint = CollitionDetectionPlayer(POINT)
+
+    if CollideWithPoint == true and POINT.collided == false then
+        SCORE = SCORE + 1
+        POINT.exists = false
+        POINT.collided = true
+    end
 end
 
 function love.draw()
-    love.graphics.print(love.timer.getFPS())
-    
     love.graphics.rectangle(
         "line",
         INITIAL_X,
@@ -46,4 +77,20 @@ function love.draw()
     )
 
     love.graphics.rectangle("fill", PLAYER.x, PLAYER.y, PLAYER.width, PLAYER.height)
+
+    if POINT.exists == true then
+        love.graphics.rectangle("fill", POINT.x, POINT.y, POINT.width, POINT.height)
+    end
+
+    love.graphics.print(SCORE, 10, 10)
+end
+
+function CollitionDetectionPlayer(collider)
+    if PLAYER.x < collider.x + collider.width and
+        PLAYER.x + PLAYER.width > collider.x and
+        PLAYER.y < collider.y + collider.height and
+        PLAYER.y + PLAYER.height > collider.y then
+        return true
+    end
+    return false
 end
